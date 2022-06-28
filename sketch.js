0f4346710fb47ba4b2f7663591cb9b9263cd8b18
VanshGhorpade <108342437+VanshGhorpade@users.noreply.github.com>
2022-06-28 17:51:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box
+
+function setup() {
+  createCanvas(600,600)
+  box=createSprite(200,200,20,20)
+  box.shapeColor="black"
+}
+function draw() {
+  background("red")
+  drawSprites()
+}
+
+    
+
+
+
+
+
